eb74b2720ec71148db0d00db5e96cd30425dc53e
Souparno <souparna2000shee@gmail.com>
2025-01-03 18:40:45 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 import { ContextProvider } from './context/useContext.jsx'
 
 
-export const server = 'http://localhost:8080/api/v1'
+export const server = 'https://worldkingv1-backend.onrender.com/api/v1'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
